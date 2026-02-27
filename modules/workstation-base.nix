60cdca5b4ca328e59a98be549060f94d25233416
Michael Slack <mike@mslack.com>
2026-02-27 15:11:14 +0800

    fix fcitx5 config

--- modules/workstation-base.nix
+++ modules/workstation-base.nix
@@ -23,13 +23,16 @@
     enable = true;
     type = "fcitx5";
     fcitx5.waylandFrontend = true;
-    fcitx5.addons = with pkgs.qt6Packages; [
-      fcitx5-rime
-      fcitx5-gtk
-      fcitx5-chinese-addons
-      fcitx5-qt
-      fcitx5-wayland
-    ];
+    fcitx5.addons =
+      (with pkgs; [
+        fcitx5-rime
+        fcitx5-gtk
+      ])
+      ++ (with pkgs.qt6Packages; [
+        fcitx5-chinese-addons
+        fcitx5-qt
+        fcitx5-wayland
+      ]);
   };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
