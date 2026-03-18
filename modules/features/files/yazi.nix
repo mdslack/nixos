1fbb272a3cac33f603f80bc4b72e62d746a95044
Michael Slack <mike@mslack.com>
2026-03-18 10:35:50 +0800

    update yazi configuration

--- modules/features/files/yazi.nix
+++ modules/features/files/yazi.nix
@@ -1,45 +1,43 @@
 _: {
-  flake.modules.nixos.files-yazi =
-    { pkgs, ... }:
-    {
-      environment.systemPackages = [
-        pkgs.yazi
-      ];
-    };
+  flake.modules.nixos.files-yazi = {pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
+      yazi
+      poppler
+      resvg
+      zoxide
+    ];
+  };
 
-  flake.modules.homeManager.files-yazi =
-    { pkgs, ... }:
-    let
-      yaziCatppuccinMocha = pkgs.stdenvNoCC.mkDerivation {
-        pname = "yazi-flavor-catppuccin-mocha";
-        version = "2026-01-18";
+  flake.modules.homeManager.files-yazi = {pkgs, ...}: let
+    yaziCatppuccinMocha = pkgs.stdenvNoCC.mkDerivation {
+      pname = "yazi-flavor-catppuccin-mocha";
+      version = "2026-01-18";
 
-        src = pkgs.fetchFromGitHub {
-          owner = "yazi-rs";
-          repo = "flavors";
-          rev = "ca6165818bb84d46af5fd8f95bedd2b1c395890a";
-          hash = "sha256-xGnebGuSOZpQl/QhuZkwgrjfAlfbEtruA9UVe030mZM=";
-        };
-
-        installPhase = ''
-          mkdir -p "$out"
-          cp "$src/catppuccin-mocha.yazi"/* "$out/"
-        '';
+      src = pkgs.fetchFromGitHub {
+        owner = "yazi-rs";
+        repo = "flavors";
+        rev = "ca6165818bb84d46af5fd8f95bedd2b1c395890a";
+        hash = "sha256-xGnebGuSOZpQl/QhuZkwgrjfAlfbEtruA9UVe030mZM=";
       };
-    in
-    {
-      programs.yazi = {
-        enable = true;
-        shellWrapperName = "yy";
-        flavors = {
-          catppuccin-mocha = yaziCatppuccinMocha;
-        };
-        theme = {
-          flavor = {
-            dark = "catppuccin-mocha";
-            light = "catppuccin-mocha";
-          };
+
+      installPhase = ''
+        mkdir -p "$out"
+        cp "$src/catppuccin-mocha.yazi"/* "$out/"
+      '';
+    };
+  in {
+    programs.yazi = {
+      enable = true;
+      shellWrapperName = "yy";
+      flavors = {
+        catppuccin-mocha = yaziCatppuccinMocha;
+      };
+      theme = {
+        flavor = {
+          dark = "catppuccin-mocha";
+          light = "catppuccin-mocha";
         };
       };
     };
+  };
 }
