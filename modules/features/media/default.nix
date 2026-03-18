49b27f723b102ed30e349e890d928e864ab99ea1
Michael Slack <mike@mslack.com>
2026-03-18 10:25:24 +0800

    add media apps

--- modules/features/media/default.nix
+++ modules/features/media/default.nix
@@ -1,8 +1,19 @@
 {config, ...}: {
   flake.modules.nixos.media = {
     imports = [
+      config.flake.modules.nixos.media-graphics
       config.flake.modules.nixos.media-recording
       config.flake.modules.nixos.media-spotify
+    ];
+  };
+
+  flake.modules.nixos.media-graphics = {pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
+      imagemagick
+      gimp
+      inkscape
+      krita
+      zathura
     ];
   };
 }
