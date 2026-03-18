29e2b72b57d20f308cfe03e9afb2b82d521b7b2f
Michael Slack <mike@mslack.com>
2026-03-18 12:43:05 +0800

    update noctalia flake and add swayidle lock/suspend
    This flake update seems to fix the lock/suspend bug we encountered
    previously. Not sure exactly what got fixed, but it appears to be
    working properly now.

--- modules/features/session/noctalia.nix
+++ modules/features/session/noctalia.nix
@@ -1,28 +1,50 @@
-{ inputs, ... }:
-{
-  flake.modules.nixos.session-noctalia = {
+{inputs, ...}: {
+  flake.modules.nixos.session-noctalia = {pkgs, ...}: {
     imports = [
       inputs.noctalia.nixosModules.default
     ];
 
+    environment.systemPackages = [
+      pkgs.swayidle
+    ];
+
+    systemd.user.services.swayidle = {
+      description = "Swayidle Service";
+      after = ["graphical-session.target"];
+      wantedBy = ["graphical-session.target"];
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = ''
+          ${pkgs.swayidle}/bin/swayidle -w \
+            timeout 300 "${pkgs.quickshell}/bin/qs -c noctalia-shell ipc call lockScreen lock" \
+            timeout 600 "${pkgs.quickshell}/bin/qs -c noctalia-shell ipc call sessionMenu lockAndSuspend"
+        '';
+        Restart = "on-failure";
+        TimeoutSec = 30;
+      };
+    };
+
     services.noctalia-shell.enable = true;
   };
 
-  flake.modules.homeManager.session-noctalia =
-    { config, lib, ... }:
-    {
-      imports = [
-        inputs.noctalia.homeModules.default
-      ];
+  flake.modules.homeManager.session-noctalia = {
+    config,
+    lib,
+    ...
+  }: {
+    imports = [
+      inputs.noctalia.homeModules.default
+    ];
 
-      programs.noctalia-shell = {
-        enable = true;
-        settings = lib.mkForce { };
-      };
-
-      xdg.configFile."noctalia" = {
-      	source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/noctalia";
-	recursive = true;
-      };
+    programs.noctalia-shell = {
+      enable = true;
+      settings = lib.mkForce {};
     };
+
+    xdg.configFile."noctalia" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/noctalia";
+      recursive = true;
+    };
+  };
 }
