3a54e31021de3f9fcbfa31d8079d0385f9fa771c
Michael Slack <mike@mslack.com>
2026-04-23 12:41:50 -0500

    update prometheus configuration

--- modules/features/monitoring/prometheus.nix
+++ modules/features/monitoring/prometheus.nix
@@ -4,6 +4,21 @@ _: {
       enable = true;
       listenAddress = "127.0.0.1";
       port = 9090;
+      extraFlags = [ "--web.enable-admin-api" ];
+      remoteWrite = [
+        {
+          url = "http://127.0.0.1:8428/api/v1/write";
+          queue_config = {
+            capacity = 10000;
+            min_shards = 1;
+            max_shards = 4;
+            max_samples_per_send = 2000;
+            batch_send_deadline = "5s";
+            min_backoff = "1s";
+            max_backoff = "30s";
+          };
+        }
+      ];
 
       scrapeConfigs = [
         {
