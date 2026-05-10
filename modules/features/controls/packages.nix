dca7647f58d3c7137d10beeb816fcb747fe6c4eb
Michael Slack <mike@mslack.com>
2026-05-10 12:13:34 -0500

    add controls apps for key light and videocams

--- modules/features/controls/packages.nix
+++ modules/features/controls/packages.nix
@@ -2,9 +2,12 @@ _: {
   flake.modules.nixos.controls-packages =
     { pkgs, ... }:
     {
+      users.users.mslack.extraGroups = [ "video" ];
+
       environment.systemPackages = with pkgs; [
         bluetui
         impala
+        v4l-utils
         wiremix
       ];
     };
