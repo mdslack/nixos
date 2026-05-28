111d5a22bb436eba727f8edbb6a8d925d0414fdc
Michael Slack <mike@mslack.com>
2026-05-27 20:24:46 -0500

    update lazygit configuration

--- modules/features/git/default.nix
+++ modules/features/git/default.nix
@@ -1,24 +1,29 @@
 _: {
-  flake.modules.homeManager.git = {
-    programs.git = {
-      enable = true;
-      lfs.enable = true;
-      settings = {
-        user = {
-          name = "Michael Slack";
-          email = "mike@mslack.com";
-        };
-        init.defaultBranch = "main";
-        core = {
-          autocrlf = false;
-          ignorecase = false;
+  flake.modules.homeManager.git =
+    { config, ... }:
+    {
+      xdg.configFile."lazygit/config.yml".source =
+        config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/lazygit/config.yml";
+
+      programs.git = {
+        enable = true;
+        lfs.enable = true;
+        settings = {
+          user = {
+            name = "Michael Slack";
+            email = "mike@mslack.com";
+          };
+          init.defaultBranch = "main";
+          core = {
+            autocrlf = false;
+            ignorecase = false;
+          };
         };
       };
-    };
 
-    programs.gh = {
-      enable = true;
-      gitCredentialHelper.enable = true;
+      programs.gh = {
+        enable = true;
+        gitCredentialHelper.enable = true;
+      };
     };
-  };
 }
